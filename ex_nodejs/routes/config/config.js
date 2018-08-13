2a6640a0d78efe50fd4feb648a45b636df5b36e6
pci2676 <pci2676@gmail.com>
2018-08-13 18:27:41 +0900

    add ex_nodejs 1

--- ex_nodejs/routes/config/config.js
+++ ex_nodejs/routes/config/config.js
@@ -1,5 +1,12 @@
 const secret = 'shemchlrkdqkrcksdls';
-
+const transport = {
+  service:'gmail',
+  auth:{
+    user:'pci2676@gmail.com',
+    pass:'비밀번호'
+  }
+};
 module.exports={
-  'secret':secret
+  'secret':secret,
+  'transport':transport
 };
